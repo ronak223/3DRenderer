5c2fc0d24822e286a062025a78fe6ea341b47667
Ronak <ronak223@gmail.com>
2013-12-06 14:46:05 -0500

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+Application to render 3D Objects in Android
---
+++ gen/edu/wm/cs/new3drenderer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.wm.cs.new3drenderer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
